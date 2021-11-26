ba738d9ae9c2d21733b3187567be689392c049dc
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2021-11-26 15:00:50 +0100

    Formulare für Registrierung und Anmeldung geschrieben. Wechsel zwischen Formularen einfach möglich

--- To-Do-List/src/main/java/stellarbytestudios/todolist/webcontroller/AnmeldeRegistrierungsController.java
+++ To-Do-List/src/main/java/stellarbytestudios/todolist/webcontroller/AnmeldeRegistrierungsController.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.undertow.UndertowWebServer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import stellarbytestudios.todolist.datacontainer.User;
 
 @Controller
@@ -11,10 +12,23 @@ public class AnmeldeRegistrierungsController {
 
 
     @GetMapping("/")
-    public String Registrierung(Model m){
-
-        //User defaultUser = new User(1, "Gib hier deinen Namen ein");
-        //m.addAttribute(defaultUser);
+    public String startseiteRegistrierung(Model m){
         return "RegistrierungsForm";
     }
+
+    @PostMapping("/registrieren")
+    public String registrieren(Model m){
+        return "ErfolgreichRegistriert";
+    }
+
+    @GetMapping("/anmeldeform")
+    public String anmeldeseite(Model m){
+        return "AnmeldeForm";
+    }
+
+    @PostMapping("/anmeldung")
+    public String anmeldung(Model m){
+        return "ErfolgreichAngemeldet";
+    }
+
 }

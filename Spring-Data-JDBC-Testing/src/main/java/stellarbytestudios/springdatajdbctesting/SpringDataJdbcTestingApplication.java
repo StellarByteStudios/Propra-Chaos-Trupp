0f4bb1128b2efeefed97f6a2a4c8eec1db1b3f39
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2021-12-07 10:56:34 +0100

    SpringDataJDBC: Neues Projekt um neue Lib zu üben

---
+++ Spring-Data-JDBC-Testing/src/main/java/stellarbytestudios/springdatajdbctesting/SpringDataJdbcTestingApplication.java
@@ -0,0 +1,13 @@
+package stellarbytestudios.springdatajdbctesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJdbcTestingApplication.class, args);
+    }
+
+}
---
+++ Spring-Data-JDBC-Testing/src/test/java/stellarbytestudios/springdatajdbctesting/SpringDataJdbcTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package stellarbytestudios.springdatajdbctesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
